47ff5433228b6ac1e9d258864f1f2d14f5930990
WilmerSoriano <Official_Wilmer.2020@outlook.com>
2025-02-19 00:25:13 -0600

    arguments passes through with no problem

--- 8PuzzleSolver/expense_8_puzzle.py
+++ 8PuzzleSolver/expense_8_puzzle.py
@@ -1,24 +1,45 @@
 import os
+import sys
 
-#list of available Algorithms
+# List of available Algorithms
 
-def BFS():
+def BFS(flag):
+   print("Hello BFS" + flag)
+def UCS(flag):
+   print("Hello UCS")
+def Greedy(flag):
+   print("Hello greedy")
+# Default Algorithm
+def A_Star(flag):
+   print("Hello A*")
 
-def UCS():
 
-def Greedy():
+# Verify file and determine User input
+def main():
+   inputs = sys.argv
 
-#default Algorithm
-def A_Star():
-
-
-#verify file and determine User input
-def main(*args):
-   #1st catch exception:
-   if args[0] != "start.txt":
+   # 1st. Catch exception:
+   if  inputs[1] != "start.txt":
       print("Error: Start file missing")
       return -1
-   elif args[1] != "goal.txt":
+   elif  inputs[2] != "goal.txt":
       print("Error: Goal file mssing")
       return -1
    
+
+
+   # 2nd. Check identify Algorithm
+   match inputs[3]:
+      case "bfs":
+         BFS(inputs[4])
+      case "ucs":
+         UCS(inputs[4])
+      case "greedy":
+         Greedy(inputs[4])
+      case "a*":
+         A_Star(inputs[4])
+      case _:
+         A_Star(inputs[4])
+
+if __name__ == "__main__":
+    main()
