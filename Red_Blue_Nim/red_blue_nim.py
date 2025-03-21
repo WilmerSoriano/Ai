810ed10509b2f388daaf4d4031e52d0564d97337
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-03-20 22:50:21 -0500

    Added a comment with plan and updated input argument

--- Red_Blue_Nim/red_blue_nim.py
+++ Red_Blue_Nim/red_blue_nim.py
@@ -1,22 +1,33 @@
 import sys
 
 """
-   NOTE:
-
+   NOTE:   
+   
+   Points:
    Red marbel = 2 points each
    Blue marbel = 3 points each
+   if std version then they are (-) points (Bad)
+   if misere then they are  (+) points (Good)
 
-   if std version then they are - points
-   if misere then they are  points
+   Algorithm: 
+   MinMax with Alpha Beta Pruning for Agent player
+   (aka: the computer)
 
-   Algorithm: MinMax with Alpha Beta Pruning
+   Game:
+   When Either player (Computer and Human) is their return,
+   they're allow to pick 1 or 2 marble from the pile.
+
+   End-Game:
+   When either pile (Red or Blue Marble) is empty 
+   (with Zero remaining) the remain marble become the
+   points (Either good or bad) each. 
 """
 
-# [num of Red] [num of Blue] [game version] [human or computer]
+# [num of Red] [num of Blue] [game version] [human or computer] 
 def nim(red, blue, version, player):
    print(red, blue, version, player)
 
-#Build the game, build the agent, Micro SD then build the Algorithm.
+#Build the game, build the agent, Micro SD then build the Algorithm
 
 # 1. Check all user arguments 
 if __name__ == "__main__":
