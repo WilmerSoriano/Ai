8de9df3f841bf3ecaf6c3bb257ded44d367b6990
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-03-20 22:49:06 -0500

    Added comments and changed input arguments

--- Red_Blue_Nim/red_blue_nim.py
+++ Red_Blue_Nim/red_blue_nim.py
@@ -1,9 +1,22 @@
 import sys
 
+"""
+   NOTE:
+
+   Red marbel = 2 points each
+   Blue marbel = 3 points each
+
+   if std version then they are - points
+   if misere then they are  points
+
+   Algorithm: MinMax with Alpha Beta Pruning
+"""
 
 # [num of Red] [num of Blue] [game version] [human or computer]
 def nim(red, blue, version, player):
    print(red, blue, version, player)
+
+#Build the game, build the agent, Micro SD then build the Algorithm.
 
 # 1. Check all user arguments 
 if __name__ == "__main__":
